4ee631367e74a779a03147ecf86ce48cb037c375
Alex Hedley <alex.hedley@me.com>
2020-03-31 16:40:45 +0100

    Added youtube embed

--- js/app.js
+++ js/app.js
@@ -18,3 +18,10 @@ myApp.controller('myController', function ($scope, $http, $q, $filter) {
 
     $scope.init();
 });
+
+myApp.filter('trusted', ['$sce', function ($sce) {
+    return function(video_id) {
+        //var video_id = url.split('v=')[1].split('&')[0];
+        return $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + video_id);
+    };
+}]);
